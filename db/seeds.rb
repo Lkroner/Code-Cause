784a1f1d72935e31cb5d0dc3fa86a22aa479c364
Alex Birdsall <ambirdsall@gmail.com>
2014-10-18 13:28:47 -0700

    Seeds.rb makes 50 causes.

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,24 @@ require 'faker'
 def one_in_eight_chance
   Array.new(7, false).push(true).sample
 end
+
+50.times do
+  Cause.create!(name: Faker::Company.name,
+                                description: Faker::Company.catchphrase,
+                                help_needed: Faker::Company.bs,
+                                web_design: one_in_eight_chance,
+                                web_development: one_in_eight_chance,
+                                ios_development: one_in_eight_chance,
+                                android_development: one_in_eight_chance,
+                                data_analysis: one_in_eight_chance,
+                                algorithm_design: one_in_eight_chance,
+                                ruby_on_rails: one_in_eight_chance,
+                                django: one_in_eight_chance,
+                                html_css: one_in_eight_chance,
+                                ruby: one_in_eight_chance,
+                                python: one_in_eight_chance,
+                                javascript: one_in_eight_chance,
+                                java: one_in_eight_chance,
+                                cpp: one_in_eight_chance,
+                                c: one_in_eight_chance)
+end
