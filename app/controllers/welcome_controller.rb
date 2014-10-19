1961602e59806be46b844ac353dca7297fea835f
John <johnakersemail@gmail.com>
2014-10-19 12:48:45 -0700

    sign up routing correct, need to fix validations

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,9 @@ class WelcomeController < ApplicationController
 	def index
     @causes = Cause.take(5)
 
+  end
+
+  def signup
     @coder = Coder.new
     @do_gooder = DoGooder.new
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   resources :causes
   resources :do_gooders
   resources :coders
+
+  get '/signup', to: 'welcome#signup', as: "signup"
 end
